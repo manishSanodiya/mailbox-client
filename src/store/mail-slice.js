5153993cc8411d0eabb60b1b340794d2a15d26a0
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-05-27 08:59:23 +0530

    action

--- src/store/mail-slice.js
+++ src/store/mail-slice.js
@@ -1,33 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-// const initialState = {mailData:[], unreadMessageCount:0, firstTime:true};
 const initialState = { mailData: [], firstTime: true, unreadMessageCount: 0 };
 
 const mailSlice = createSlice({
-    name: 'mail',
-    initiaState: initialState,
-    reducers:{
-        firstTime(state,action){
-            state.firstTime = action.payload
-        },
-        add(state,action){
-            state.mailData = [action.payload, ...state.mailData]
-
-        },
-        remove(state,action){
-            state.mailData = state.mailData.filter(mail => mail.id !== action.payload.id);
-        },
-        replace(state, action) {
-            state.mailData = action.payload.mailData;
-            state.firstTime = false;
-            // console.log(state.mailData);
-            state.unreadMessageCount = action.payload.unreadMessageCount;
-          }
-    }
-})
-
-
+  name: 'mail',
+  initialState: initialState,
+  reducers: {
+    firstTime(state, action) {
+      state.firstTime = action.payload;
+    },
+    replace(state, action) {
+      state.mailData = action.payload.mailData;
+      state.firstTime = false;
+      // console.log(state.mailData);
+      state.unreadMessageCount = action.payload.unreadMessageCount;
+    },
+    add(state, action) {
+      state.mailData = [action.payload, ...state.mailData];
+      // console.log(state.mails);
+    },
+    remove(state, action) {
+        state.mailData = state.mailData.filter(mail => mail.id !== action.payload.id);
+      },
+  },
+});
 
 export const mailActions = mailSlice.actions;
-
 export default mailSlice.reducer;
